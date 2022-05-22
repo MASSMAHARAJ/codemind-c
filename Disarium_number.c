2fabed7d383daccc63affc99403d81745dbef617
SRI KARTHEEK <97425080+MASSMAHARAJ@users.noreply.github.com>
2022-05-22 20:59:47 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,alt,d,dc=0,sum=0;     scanf("%d",&n);     alt=n;     while(n)     {         dc++;         d=n%10;         n=n/10;     }     n=alt;     while(n)     {         d=n%10;         n=n/10;         sum=sum+pow(d,dc);         dc--;     }     if(sum==alt)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Disarium_number.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,alt,d,dc=0,sum=0;
+    scanf("%d",&n);
+    alt=n;
+    while(n)
+    {
+        dc++;
+        d=n%10;
+        n=n/10;
+    }
+    n=alt;
+    while(n)
+    {
+        d=n%10;
+        n=n/10;
+        sum=sum+pow(d,dc);
+        dc--;
+    }
+    if(sum==alt)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
