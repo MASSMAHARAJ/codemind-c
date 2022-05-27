c1f1dfa0305a83c9d2dc7e51268399959315f4d5
SRI KARTHEEK <97425080+MASSMAHARAJ@users.noreply.github.com>
2022-05-27 11:18:56 +0530

    #include<stdio.h> int isprime(int n) {     	int i;     	if(n==1)     	{     	   	return 0;     	}     	else     	{ 	    	for(i=2;i<n/2;i++) 	    	{ 	        	if(n%i==0) 	        	{ 	            	return 0; 	        	} 	    	} 	    	return 1;     	} } int main() { 	int n,i,j,c1=0,c2=0,c=0; 	scanf("%d",&n); 	for(i=2;i<=n/2;i++) 	{ 		if(c==1) 		{ 			break; 		} 		if(isprime(i)) 		{ 			for(j=2;j<=n/2;j++) 			{ 				if(isprime(j)) 				{ 					if(i*j==n && i<=j) 					{ 						c1++; 						printf("%d %d ",i,j); 						break; 						c=1; 						c2++; 					} 				} 			} 		} 	} 	if(c1==c2) 	{ 		printf("-1"); 	} }

--- Find_two_distinct_prime_numbers_with_given_product.c
+++ Find_two_distinct_prime_numbers_with_given_product.c
@@ -1,39 +1,53 @@
 #include<stdio.h>
-int ifprime(int n)
+int isprime(int n)
 {
-    int i;
-    for(i=2;i<n;i++)
-    {
-        if(n%i==0)
-        {
-            return 0;
-        }
-    }
-    return 1;
+    	int i;
+    	if(n==1)
+    	{
+    	   	return 0;
+    	}
+    	else
+    	{
+	    	for(i=2;i<n/2;i++)
+	    	{
+	        	if(n%i==0)
+	        	{
+	            	return 0;
+	        	}
+	    	}
+	    	return 1;
+    	}
 }
 int main()
 {
-    int i,n,k,c1=0,c2=0;
-    scanf("%d",&n);
-    for(i=2;i<n/2;i++)
-    {
-        c1++;
-        if(ifprime(i))
-        {
-            k=n/i;
-            if(ifprime(k))
-            {
-                if(i*k==n)
-                {
-                    printf("%d %d",i,k);
-                    break;
-                }
-            }
-        }
-        c2++;
-    }
-    if(c1==c2)
-    {
-        printf("-1");
-    }
+	int n,i,j,c1=0,c2=0,c=0;
+	scanf("%d",&n);
+	for(i=2;i<=n/2;i++)
+	{
+		if(c==1)
+		{
+			break;
+		}
+		if(isprime(i))
+		{
+			for(j=2;j<=n/2;j++)
+			{
+				if(isprime(j))
+				{
+					if(i*j==n && i<=j)
+					{
+						c1++;
+						printf("%d %d ",i,j);		
+						break;
+						c=1;
+						c2++;
+					}
+				}
+			}
+		}
+	}
+	if(c1==c2)
+	{
+		printf("-1");
+	}
 }
