75077c1004e70edec2c99f736b601e91089cd073
SRI KARTHEEK <97425080+MASSMAHARAJ@users.noreply.github.com>
2022-05-30 11:13:58 +0530

    #include<stdio.h> int main() {     int n,a[100],i,r=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(i%2==0)         {             r=r-a[i];         }         else         {             r=r+a[i];         }     }     printf("%d",r*-1); }

---
+++ Absolute_difference_b/w_sum_of_even_and_sum_of_odd_indexed_elements.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[100],i,r=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(i%2==0)
+        {
+            r=r-a[i];
+        }
+        else
+        {
+            r=r+a[i];
+        }
+    }
+    printf("%d",r*-1);
+}
